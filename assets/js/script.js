785a678839a2fdd70f8556d44c107b13f1570e62
SimGriff <simgrif@gmail.com>
2022-03-13 14:15:42 +0000

    Questions array added to js.

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// My list  of Questions
+const questions = [
+    {
+        question: "What are the androids in Bladerunner called?",
+        answers: [
+            { text: "Doubles", correct: false },
+            { text: "Replicants", correct: true },
+            { text: "Cylons", correct: false },
+            { text: "Carbon copies", correct: false },
+        ]
+    },
+    {
+        question: "Which of these popular sci-fi movie franchises has the most films?",
+        answers: [
+            { text: "Star Trek", correct: true },
+            { text: "Star Wars", correct: false },
+            { text: "The Matrix", correct: false },
+            { text: "Terminator", correct: false },
+        ]
+    },
+];
+console.log (questions);
